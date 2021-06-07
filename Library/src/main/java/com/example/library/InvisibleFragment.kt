8f02c076ceb7108120b6d4f9fc2a1cb4f59499b2
LYX <944050317@qq.com>
2021-06-07 15:35:53 +0800

    First commit

---
+++ Library/src/main/java/com/example/library/InvisibleFragment.kt
@@ -0,0 +1,29 @@
+package com.example.library
+
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+class InvisibleFragment: Fragment() {
+    private var callback: ((Boolean,List<String>) -> Unit)? = null
+    fun requestNow(cb: (Boolean,List<String>) -> Unit,vararg permissions: String){
+        callback = cb
+        requestPermissions(permissions,1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == 1){
+            val deniedList = ArrayList<String>()
+            for((index,result) in grantResults.withIndex()){
+                if(result != PackageManager.PERMISSION_GRANTED){
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted = deniedList.isEmpty()
+            callback?.let { it(allGranted,deniedList) }
+        }
+    }
+}
